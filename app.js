a4efd0be9340486a2b7241adf70d1bb737cc85ce
Lucas Isla <lisla@purdue.edu>
2017-06-08 11:21:46 -0400

    Remove flicks from the ul, but not the array

--- app.js
+++ app.js
@@ -38,42 +38,12 @@ const app = {
     item
       .querySelector('.flick-name')
       .textContent = flick.name
+    
+    item
+      .querySelector('button.remove')
+      .addEventLestener('click', this.removeFlick)
 
     return item;
-
-    // const item = document.createElement('li')
-    // const p = document.createElement('p')
-    // p.textContent = flick.name
-    // const div = document.createElement('div')
-    // div.className = 'button-group'
-    // const b1 = document.createElement('a')
-    // b1.className = 'success button'
-    // const b2 = document.createElement('a')
-    // b2.className = 'alert button'
-    // const b3 = document.createElement('a')
-    // b3.className = 'button'
-    // const b4 = document.createElement('a')
-    // b4.className = 'button'
-
-    // b1.textContent = 'Promote'
-    // b1.onclick = this.promoteItem.bind(this)
-    // b2.textContent = 'Delete'
-    // b2.onclick = this.deleteItem.bind(this)
-    // b3.innerHTML = '&#8679;'
-    // b3.onclick = this.moveItemUp.bind(this)
-    // b4.innerHTML = '&#8681;'
-    // b4.onclick = this.moveItemDown.bind(this)
-
-    // div.appendChild(b1)
-    // div.appendChild(b2)
-    // div.appendChild(b3)
-    // div.appendChild(b4)
-
-    // item.appendChild(p)
-    // item.appendChild(div)
-    // item.dataset.id = flick.id
-
-    // return item
   },
 
   promoteItem(ev){
@@ -84,6 +54,7 @@ const app = {
       .style.backgroundColor = 'gold'
     b.onclick = this.demoteItem.bind(this)
   },
+
   demoteItem(ev){
     const b = ev.target
     b.textContent = 'Promote'
@@ -92,12 +63,12 @@ const app = {
       .style.backgroundColor = 'white'
     b.onclick = this.promoteItem.bind(this)
   },
-  deleteItem(ev){
-    const b = ev.target
-    const p = b.parentNode
-    const pp = p.parentNode
-    pp.parentNode.removeChild(pp)
+
+  removeFlick(ev){
+    const listItem = ev.target.closest('.flick')
+    listItem.remove()
   },
+
   moveItemUp(ev){
     const b = ev.target
     const p = b.parentNode
@@ -106,6 +77,7 @@ const app = {
       element.parentNode.insertBefore(element, element.previousElementSibling)
     }
   },
+  
   moveItemDown(ev){
     const b = ev.target
     const p = b.parentNode
